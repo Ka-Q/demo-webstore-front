bd7fb67a4b0e30a972a9180544e596fb6f3f60c1
akulau <akulaurila@gmail.com>
2023-08-07 05:57:45 +0300

    Added styling to root

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='root-style'>
+      <App />
+    </div>
   </React.StrictMode>
 );
 
