7ad455a5e6f72baae145f018ca8537357d646894
akulau <akulaurila@gmail.com>
2023-09-13 13:48:26 +0300

    Fixed redundant css and cleaned up ImageViewer

--- src/components/ImageViewerComponent/ImageViewerComponent.js
+++ src/components/ImageViewerComponent/ImageViewerComponent.js
@@ -1,21 +1,23 @@
 import { useEffect, useState} from "react"
 import { Image, Card } from "react-bootstrap"
 
+import './ImageViewer.css';
+
 // Maps the list of preview images for ImageViewerComponent
-const mapImageToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
-    let previewList = list.map((n, index) => {
+const mapImageToPreviews = (imageList, path, clickedPic, setClickedPic) => {
+    let previewList = imageList.map((n, index) => {
         n.index = index
         if (clickedPic.image_id == n.image_id){
             return (
-                <div id={"previewPic" + index} className="mx-1" key={"previewPic" + n.image_id} style={{display:"inline-block", overflow: "auto", whiteSpace: "nowrap", height: "6em", borderRadius: "0.3em", minWidth:"4em", backgroundColor: "black"}}>
-                    <Image src={kuvaSrc + n.image_source} height={"100%"}/>
+                <div className="mx-1 carousel-preview-image" id={"previewPic" + index} key={"previewPic" + n.image_id} >
+                    <Image src={path + n.image_source} height={"100%"}/>
                 </div>
             )
         }
         return (
-            <div id={"previewPic" + index} className="mx-1" key={"previewPic" + n.image_id} onClick={(e) => setClickedPic(n)} style={{position: "relative", display:"inline-block", overflow: "auto", whiteSpace: "nowrap", height: "6em" , borderRadius: "0.3em"}}>
-                <div style={{display: "block", overflow: "auto", whiteSpace: "nowrap", height: "100%", filter: "brightness(0.4)", margin: 0, padding: 0, minWidth:"4em", backgroundColor: "black"}}>
-                    <Image src={kuvaSrc + n.image_source} height={"100%"}/>
+            <div className="mx-1 carousel-preview-image" id={"previewPic" + index} key={"previewPic" + n.image_id} onClick={() => setClickedPic(n)}>
+                <div className="carousel-preview-inactive-filter">
+                    <Image src={path + n.image_source} height={"100%"}/>
                 </div>
             </div>
         )
@@ -27,7 +29,7 @@ const mapImageToPreviews = (list, kuvaSrc, clickedPic, setClickedPic) => {
 const ImageViewerComponent = (props) => {
 
     const [clickedPic, setClickedPic] = useState(props.images[0]);
-    const [imageList, setImageList] = useState(props.images);
+    const imageList = props.images;
 
     if (!props.images || props.images.length == 0) return (
         <Card border="secondary" style={{backgroundColor: "rgb(50,50,50)", color: "white"}}>
@@ -66,21 +68,16 @@ const ImageViewerComponent = (props) => {
         scrollToPreview(-1)
     }
 
-    // Styling
-    let btnStyle = {backgroundColor: "rgba(40,40,40,0.8)", width:"4em", height: "4em", borderRadius: "1em", padding: "1em", cursor: "pointer", userSelect: "none", fontWeight: "bold", position: "absolute", top: "20em"}
-    let BtnStyleLeft = JSON.parse(JSON.stringify(btnStyle)); BtnStyleLeft.left = 0
-    let BtnStyleRight = JSON.parse(JSON.stringify(btnStyle)); BtnStyleRight.right = 0
-
     return(
         <Card className="p-2" style={{backgroundColor: "rgb(20,20,20)", border: "2px solid rgb(40,40,40)", color: "white"}}>
-            <div style={{height: "30em"}}>
-            <a onClick={(e) => window.open(props.path + clickedPic.valokuva, '_blank').focus()} style={{cursor:"pointer"}}>
-                <Image src={props.path + clickedPic.image_source} fluid style={{flexShrink: 0, objectFit: "contain", height:"100%", minWidth: "100%", backgroundColor: "black", borderRadius: "0.3em"}}/>
-            </a>
-                <div className="ms-4" style={BtnStyleLeft} onClick={(e) => handleDecrease()}>{"<"}</div>
-                <div className="me-4" style={BtnStyleRight} onClick={(e) => handleIncrease()}>{">"}</div>
+            <div style={{height: "30em", position: "relative"}}>
+                <a onClick={(e) => window.open(props.path + clickedPic.image_source, '_blank').focus()} style={{}}>
+                    <Image className="carousel-image" fluid src={props.path + clickedPic.image_source}/>
+                </a>
+                <div className="ms-4 text-center carousel-btn carousel-btn-left" onClick={() => handleDecrease()}><div style={{scale: "2", pointerEvents: "none"}}>{"←"}</div></div>
+                <div className="me-4 text-center carousel-btn carousel-btn-right" onClick={() => handleIncrease()}><div style={{scale: "2", pointerEvents: "none"}}>{"→"}</div></div>
             </div>
-            <div id="previewScroll" className="pt-2" style={{display:"inline-block", overflow: "auto", whiteSpace: "nowrap", width: "100%", height: "auto", userSelect: "none"}} >
+            <div id="previewScroll" className="pt-2 carousel-preview">
                 {previewList}
             </div>
         </Card>
