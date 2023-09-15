bf24e4487bb334dbb93e9ef923844966dae9aea0
akulau <akulaurila@gmail.com>
2023-09-15 15:59:34 +0300

    Reworked FilterComponent styling and category logic

--- src/components/pages/SearchPage/FilterComponent.js
+++ src/components/pages/SearchPage/FilterComponent.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Card, FormControl, Form } from "react-bootstrap";
+import { Button, Card, FormControl, Form, Collapse } from "react-bootstrap";
 import { hideResults } from "../../searchBarControl";
 import MultiRangeSlider from "../../multiRangeSlider/MultiRangeSlider";
 
@@ -41,15 +41,17 @@ const FilterComponent = (props) => {
     }
 
     return (
-        <Card className="sticky-top my-1 mx-auto" style={{color: "white", width: "100%", backgroundColor: "rgb(20,20,20)", border: "2px solid rgb(40,40,40)", maxWidth: "100%", top: 80, zIndex: 0, overflow: "auto", height: "80vh"}}>
+        <Card className="my-1 mx-auto" style={{color: "white", width: "100%", backgroundColor: "rgb(20,20,20)", border: "2px solid rgb(40,40,40)", maxWidth: "100%", zIndex: 0, overflow: "auto"}}>
             <div>
-                <h2 style={{textAlign: "center"}}>Filter products</h2>
+                <h2 className="mt-3" style={{textAlign: "center"}}>Filter results</h2>
                 <Form className="mx-2">
                     <FormControl placeholder="Product name" onChange={(e) => setProductName(e.target.value)} value={productName} className="bg-body-tertiary my-3" data-bs-theme="dark"/>
                     <CategoryFilterComponent searchParams={searchParams} key={"Categoryfilter" + keyNum}/>
                     <PricefilterComponent searchParams={searchParams} key={"Pricefilter" + keyNum}/>
-                    <Button onClick={() => updateQuery()}>Filter</Button> <Button onClick={() => clearFilters()}>Clear filters</Button>
-                    <div className="mb-2"/>
+                    <div className="mb-2" style={{display: "flex"}}>
+                        <Button onClick={() => clearFilters()} style={{backgroundColor: "rgb(50,50,50)"}}>Clear filters</Button>
+                        <Button onClick={() => updateQuery()} style={{}} className="ms-auto">Filter</Button>
+                    </div>
                 </Form>
             </div>
         </Card>
@@ -59,23 +61,37 @@ const FilterComponent = (props) => {
 const CategoryFilterComponent = (props) => {
     const searchParams = props.searchParams;
     const [maincategories, setMaincategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState(searchParams.getAll('category'));
+
+    const [openAll, setOpenAll] = useState(false);
+    const [openAllCounter, setOpenAllCounter] = useState(0);
 
     useEffect(() => {
         const fetchMaincategories = async () => {
-            const f = await fetch(`${API_PATH}/api/maincategory`);
+            const f = await fetch(`${API_PATH}/api/maincategory_expanded`);
             const data = await f.json();
             setMaincategories(data.data);
         }
         fetchMaincategories();
     }, []);
 
+    useEffect(() => {
+        searchParams.delete('category');
+        for (let category of selectedCategories) {
+            searchParams.append('category', category);
+        }
+    }, [selectedCategories])
+
     return (
         <div>
-            Filter by category:
+            <div style={{display: "flex"}}>
+            <span>Filter by category:</span>
+            <span className="ms-auto" onClick={() => {setOpenAll(!openAll); setOpenAllCounter(openAllCounter + 1)}}>{!openAll? "Open all" : "Collapse All"}</span>
+            </div>
             {maincategories.map((n, index) => {
                 return (
                     <div key={"maincategoryFilter" + n.maincategory_id}>
-                        <MainCategoryFilterItem main={n} searchParams={searchParams}/>
+                        <MainCategoryFilterItem main={n} selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories} searchParams={searchParams} openAll={openAll} openAllCounter={openAllCounter}/>
                     </div>
                 )
             })}
@@ -86,98 +102,90 @@ const CategoryFilterComponent = (props) => {
 const MainCategoryFilterItem = (props) => {
 
     const mainCategory = props.main;
-    const [subCategories, setSubCategories] = useState([]);
+    const subCategories = mainCategory.categories
 
-    const searchParams = props.searchParams;
+    const selectedCategories = props.selectedCategories;
+    const setSelectedCategories = props.setSelectedCategories;
+
+    const [open, setOpen] = useState(subCategories.map((n) => {return (n.category_name)}).some(cat => selectedCategories.indexOf(cat) >= 0));
 
     useEffect(() => {
-        const fetchSubCategories = async () => {
-            const f = await fetch(`${API_PATH}/api/maincategory_category?maincategory_id=${mainCategory.maincategory_id}`);
-            const data = await f.json();
-            if (data.data) {
-                setSubCategories(data.data);
-            } else {
-                setSubCategories([{category_id: 'error'}]);
-            }
+        if (subCategories.map((n) => {return (n.category_name)}).some(cat => selectedCategories.indexOf(cat) >= 0)) {
+            setOpen(true);
+        } else {
+            setOpen(false);
         }
-        fetchSubCategories();
-    }, [])
+
+    }, [props.searchParams])
+
+    useEffect(() => {
+        if (props.openAllCounter > 0) setOpen(props.openAll);
+    }, [props.openAllCounter])
 
     const toggleMainCategory = (e) => {
-        if(e.target.checked) {
-            for (let cat of subCategories) {
-                let checkboxes = document.querySelectorAll(`#categoryCheck${mainCategory.maincategory_id}-${cat.category_id}`);
-                for (let checkbox of checkboxes) {
-                    checkbox.checked = true;
-                    searchParams.append('category', cat.category_name);
-                }
-            }
+
+        const isChecked = e.target.checked;
+        if (isChecked) {
+            setSelectedCategories([...selectedCategories, ...subCategories.map((n) => {return (n.category_name)})]);
+            setOpen(true);
         } else {
-            for (let cat of subCategories) {
-                let checkboxes = document.querySelectorAll(`#categoryCheck${mainCategory.maincategory_id}-${cat.category_id}`);
-                for (let checkbox of checkboxes) {
-                    checkbox.checked = false;
-                    searchParams.delete('category');
-                }
-            }
+            const newSelectedCategories = selectedCategories.filter((c) => {
+                return !subCategories.map((n) => {return (n.category_name)}).includes(c);
+            });
+            console.log(newSelectedCategories);
+            setSelectedCategories(newSelectedCategories);
+            setOpen(true);
         }
     }
 
     return (
-        <div className="my-1 pb-2" style={{borderBottom: "1px solid rgb(100,100,100)"}}>
-            <input 
-                type="checkbox" 
-                name={mainCategory.maincategory_name + "mainCategoryCheck"} 
-                id={`mainCategoryCheck${mainCategory.maincategory_id}`} 
-                className="filter-checkbox"
-                value={mainCategory.maincategory_name}
-                onChange={(e) => toggleMainCategory(e)}
-            />
-            <label htmlFor={"mainCategoryCheck" + mainCategory.maincategory_id}>{mainCategory.maincategory_name}</label><br/>
-            {subCategories.map((n, index) => {
-                return (<CategoryFilterItem key={"categoryFilterItem" + n.category_id} category={n} searchParams={searchParams} mainCategory={mainCategory}/>)
-            })}
+        <div className="my-1 pb-2">
+            <div style={{display: "flex", backgroundColor: "rgb(10,10,10)", borderTop: "1px solid rgb(50,50,50)", lineHeight: "2em", cursor: "pointer"}}  onClick={() => setOpen(!open)}>
+                <input 
+                    type="checkbox" 
+                    name={mainCategory.maincategory_name + "mainCategoryCheck"} 
+                    id={`mainCategoryCheck${mainCategory.maincategory_id}`} 
+                    className="filter-checkbox"
+                    value={mainCategory.maincategory_name}
+                    checked={subCategories.map((n) => {return (n.category_name)}).every(cat => selectedCategories.includes(cat))}
+                    onChange={(e) => toggleMainCategory(e)}
+                />
+                <label htmlFor={"mainCategoryCheck" + mainCategory.maincategory_id}>{mainCategory.maincategory_name}</label>
+                <span className="ms-auto px-3" style={{cursor: "pointer", userSelect: "none"}}>{open? "hide ▲" : "show ▼"}</span><br/>
+            </div>
+            <Collapse in={open}>
+                <div style={{border: "3px solid rgb(10,10,10)"}}>
+                    {subCategories.map((n, index) => {
+                        return (<CategoryFilterItem key={"categoryFilterItem" + n.category_id} category={n} mainCategory={mainCategory} selectedCategories={selectedCategories} setSelectedCategories={setSelectedCategories}/>)
+                    })}
+                </div>
+            </Collapse>
         </div>
     )
 }
 
 const CategoryFilterItem = (props) => {
-    const searchParams = props.searchParams;
     const category = props.category;
-
-    let filtered = false;
-    for (let param of searchParams.getAll('category')) {
-        if (param == category.category_name) {
-            filtered = true;
-            break;
-        }
-    }
-
-    const toggleCategory = (e) => {
-        let categories = searchParams.getAll('category');
-
-        if (e.target.checked) {
-            categories.push(e.target.value);
-        } 
-        else {
-            categories = categories.filter(category => category !== e.target.value);
-        }
-        searchParams.delete('category');
-        for (const category of categories) {
-            searchParams.append('category', category);
-        }
-        
-    }
+    const selectedCategories = props.selectedCategories;
+    const setSelectedCategories = props.setSelectedCategories;
 
     return (
         <div className="ms-4" key={"categoryFilter" + category.category_name}>
             <input 
-                defaultChecked={filtered}
                 type="checkbox" name={category.category_name + "Radio"} 
                 id={`categoryCheck${props.mainCategory.maincategory_id}-${category.category_id}`} 
                 className="filter-checkbox"
                 value={category.category_name}
-                onChange={(e) => toggleCategory(e)}
+                checked={selectedCategories.includes(category.category_name)}
+                onChange={(event) => {
+                    const isChecked = event.target.checked;
+                    if (isChecked) {
+                        setSelectedCategories([...selectedCategories, category.category_name]);
+                    } else {
+                        const newSelectedCategories = selectedCategories.filter((c) => c !== category.category_name);
+                        setSelectedCategories(newSelectedCategories);
+                    }
+                }}
             />
             <label htmlFor={`categoryCheck${props.mainCategory.maincategory_id}-${category.category_id}`}>{category.category_name}</label><br/>
         </div>
@@ -225,6 +233,5 @@ const PricefilterComponent = (props) => {
       </div>
     )
   }
-  
 
 export default FilterComponent;
